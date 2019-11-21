9fb58e543d7ae0795203bbe2530acf93e7842994
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-11-20 19:42:45 -0600

    Autores

--- index.php
+++ index.php
@@ -9,6 +9,34 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <title>H-Library</title>
+    <style type="text/css">
+      nav li ul{
+        display:none;
+      }
+      nav li a{
+        display: block;
+      }
+      nav li a:hover{
+        background-color: #CECEC0;
+      }
+      .submenu{
+        position: absolute;
+      }
+      .submenu li a{
+        display: block;
+        background-color: #F1F1EE;
+      }
+      nav li:hover > ul{
+        display: block;
+      }
+      nav li ul li {
+        position: relative;
+      }
+      nav li ul li ul{
+        right:140px;
+        top:0px;
+      }
+    </style>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -26,6 +54,17 @@
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#" tabindex="1">Obras y artistas</a>
+        <ul class ="submenu">
+          <li><a class="nav-link" href="#" tabindex="10">Artistas</a>
+          <ul class ="personas">
+            <li><a class="nav-link" href="gabriela.html" tabindex="14">Gabriel García Márquez </a></li>
+            <li><a class="nav-link" href="#" tabindex="15">Charles Dickens </a></li>
+            </li>
+          </ul>
+          <li><a class="nav-link" href="#" tabindex="11">Categorías</a></li>
+          <li><a class="nav-link" href="#" tabindex="12">Página de colecciones de artistas</a></li>
+          <li><a class="nav-link" href="#" tabindex="13">Página de publicaciones</a></li>
+        </ul> 
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#" tabindex="2">Visitas</a>
