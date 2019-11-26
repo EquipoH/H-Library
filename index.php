54812cdebc59744a63759e6f8d6034e13e058cae
Sydney Ayala <sydneyayala1305@gmail.com>
2019-11-25 23:35:31 -0600

    Login como index 2

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<HTML>
+﻿<HTML>
 	<TITLE>
 		Inicio K
 	</TITLE>
@@ -17,9 +17,9 @@
 				<BR></BR>
 				<BR></BR>
 				<BR></BR>
-				<INPUT TYPE="BUTTON" VALUE="Iniciar sesión" ONCLICK="window.location.href='iniciarSesion.php'"></INPUT>
+				<INPUT TYPE="BUTTON" VALUE="Iniciar sesión" ONCLICK="window.location.href='../login/iniciarSesion.php'"></INPUT>
 				<BR></BR>
-				<INPUT TYPE="BUTTON" VALUE="Registrarme" ONCLICK="window.location.href='registro.php'"></INPUT>
+				<INPUT TYPE="BUTTON" VALUE="Registrarme" ONCLICK="window.location.href='../login/registro.php'"></INPUT>
 				<BR></BR>
 			</CENTER>
 		</DIV>
