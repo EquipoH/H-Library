c8184072e934d2baa627906dbf4ac3fdd548d05b
Sydney Ayala <sydneyayala1305@gmail.com>
2019-11-26 13:27:45 -0600

    Diseño en login

--- Login/registro.php
+++ Login/registro.php
@@ -1,29 +1,162 @@
-<HTML>
-	<TITLE>
-		Registro
-	</TITLE>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
 
-	<HEAD>
-		<META HTTP-EQUIV = "Content-Type" CONTENT = "text/html; charset=utf-8"/>
-	</HEAD>
+/* Full-width input fields */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
 
-	<BODY BGCOLOR="BLUE">
+/* Set a style for all buttons */
+button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
 
-		<BR>
-		<FONT FACE = "gulim" SIZE = 7 COLOR = "WHITE"><CENTER><B>Registro</B></CENTER></FONT>
+button:hover {
+  opacity: 0.8;
+}
 
-		<FORM ACTION="registroPHP.php" METHOD="POST">
-			<DIV>
-				<CENTER>
-					<BR></BR>
-					<INPUT TYPE="TEXT" NAME="USER" PLACEHOLDER="Usuario"></INPUT>
-					<BR></BR>
-					<INPUT TYPE="PASSWORD" NAME="CONTRASENA" PLACEHOLDER="Contraseña"></INPUT>
-					<BR></BR>
-					<INPUT TYPE="SUBMIT" VALUE="OK" CLASS="elementos"></INPUT>
-					<BR></BR>
-				</CENTER>
-			</DIV>
-		</FORM>
-	</BODY>
-</HTML>
+/* Extra styles for the cancel button */
+.cancelbtn {
+  width: auto;
+  padding: 10px 18px;
+  background-color: #f44336;
+}
+
+/* Center the image and position the close button */
+.imgcontainer {
+  text-align: center;
+  margin: 24px 0 12px 0;
+  position: relative;
+}
+
+img.avatar {
+  width: 40%;
+  border-radius: 50%;
+}
+
+.container {
+  padding: 16px;
+}
+
+span.psw {
+  float: right;
+  padding-top: 16px;
+}
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+  padding-top: 60px;
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+  border: 1px solid #888;
+  width: 80%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button (x) */
+.close {
+  position: absolute;
+  right: 25px;
+  top: 0;
+  color: #000;
+  font-size: 35px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: red;
+  cursor: pointer;
+}
+
+/* Add Zoom Animation */
+.animate {
+  -webkit-animation: animatezoom 0.6s;
+  animation: animatezoom 0.6s
+}
+
+@-webkit-keyframes animatezoom {
+  from {-webkit-transform: scale(0)} 
+  to {-webkit-transform: scale(1)}
+}
+  
+@keyframes animatezoom {
+  from {transform: scale(0)} 
+  to {transform: scale(1)}
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+  span.psw {
+     display: block;
+     float: none;
+  }
+  .cancelbtn {
+     width: 100%;
+  }
+}
+</style>
+</head>
+<body>
+
+  <form class="modal-content animate" action="registroPHP.php" method="post">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+      <img src="../src/logoH.jpg" alt="Avatar" class="avatar" height="320">
+      <H1>Registro de usuarios H-Library</H1>
+    </div>
+
+    <div class="container">
+      <label for="uname"><b>Usuario</b></label>
+      <input type="text" placeholder="Ingresa tu usuario" name="uname" required>
+
+      <label for="psw"><b>Contraseña</b></label>
+      <input type="password" placeholder="Ingresa tu contraseña" name="psw" required>
+        
+      <button type="submit">Registrarme</button>
+    </div>
+  </form>
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+
+</body>
+</html>
