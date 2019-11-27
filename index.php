68d790096f22f158121b719d9cfb056c0e824c49
Gabriel Rodriguez <48400046+bonnesbaby14@users.noreply.github.com>
2019-11-26 23:16:17 -0600

    sydney estas bien pendejo no pongas php al final

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <title>H-Library</title>
-  <h1>Bienvenido, <? session_start(); echo $_SESSION['name'] php?></h1>
+  <h1>Bienvenido, <? session_start(); echo $_SESSION['name'] ?></h1>
   <style type="text/css">
     nav li ul {
       display: none;
