44b6dd02ccded9e7eb90f64b705d3ff5f627b887
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-11-26 19:22:40 -0600

    soporte

--- libreria.php
+++ libreria.php
@@ -1,6 +1,6 @@
 ﻿<!DOCTYPE html>
 <html lang="es">
-
+<meta charset="UTF-8">
 <head>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <meta charset="UTF-8">
@@ -91,7 +91,7 @@
         <a class="nav-link" href="#" tabindex="3">Que hay de nuevo en la librería</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#" tabindex="4">Únete y soporte</a>
+        <a class="nav-link" href="soporte.html" tabindex="4">Únete y soporte</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#" tabindex="5">Tienda</a>
